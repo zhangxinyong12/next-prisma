3823c1e8d713164abf1c2346c7925c222b40fb6e
张新永 <xinyong.zhang@zen-x.com.cn>
2024-04-09 15:41:03 +0800

    feat: 修改菜单

---
+++ src/components/menu/index.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import MenuList from "@/data/mune"
+import { Menu, MenuProps } from "antd"
+import { usePathname, useRouter } from "next/navigation"
+
+export default function MyMenu() {
+  const router = useRouter()
+  const pathname = usePathname()
+  const selectedKeys = [pathname]
+
+  function onClick(item: any) {
+    router.push(item.key)
+  }
+
+  return (
+    <Menu
+      onClick={onClick}
+      style={{ width: 256 }}
+      mode="inline"
+      items={MenuList}
+      theme="light"
+      selectedKeys={selectedKeys}
+    />
+  )
+}
---
+++ src/data/mune.ts
@@ -0,0 +1,14 @@
+import { MenuProps } from "antd"
+
+const MenuList: MenuProps["items"] = [
+  {
+    label: "首页",
+    key: "/",
+  },
+  {
+    label: "用户",
+    key: "/user",
+  },
+]
+
+export default MenuList
